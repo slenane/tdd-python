85720ed3bf13677cb7a6f12ef00d2ce67c20d003
Stephen Lenane <slenane1@gmail.com>
2025-09-02 11:33:56 +0200

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -11,10 +11,28 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<input name="item_text"')
         
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        
+        response = self.client.get("/")
+        
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+        
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})   
-        self.assertContains(response, "A new list item")  
-        self.assertTemplateUsed(response, "home.html")
+        self.client.post("/", data={"item_text": "A new list item"}) 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+        
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/")
+        
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
         
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
