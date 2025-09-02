27cad7348b14d2c0c67ef2d975cdf9c8710ae5e0
Stephen Lenane <slenane1@gmail.com>
2025-09-02 14:43:37 +0200

    Added new url for new lists

--- lists/tests.py
+++ lists/tests.py
@@ -8,22 +8,8 @@ class HomePageTest(TestCase):
         
     def test_renders_input_form(self):
         response = self.client.get('/')
-        self.assertContains(response, '<form method="POST" action="/">')
+        self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
-        
-    def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"}) 
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-        
-    def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
-        
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
 
         
 class ItemModelTest(TestCase):
@@ -45,6 +31,18 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, "Item the second")
         
         
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"}) 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+        
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+        
+        
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
@@ -52,7 +50,7 @@ class ListViewTest(TestCase):
     
     def test_renders_input_form(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
-        self.assertContains(response, '<form method="POST" action="/">')
+        self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
 
     def test_displays_all_list_items(self):
