7c17e15cd6ab00bea5d2c6dd44fbc74ba636ae52
Stephen Lenane <slenane1@gmail.com>
2025-09-03 14:26:53 +0200

    Split out unit tests into two files

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -15,35 +15,6 @@ class HomePageTest(TestCase):
         inputs = form.cssselect("input")
         self.assertIn("item_text", [input.get("name") for input in inputs])
 
-        
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
-        
-        first_item =  Item()
-        first_item.text = "The first (ever) item"
-        first_item.list = mylist
-        first_item.save()
-        
-        second_item =  Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-        
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-        
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) item")
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
-
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
