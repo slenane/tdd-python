94b2111db60290eac0e9fbbb2e28e7c42d071199
Stephen Lenane <slenane1@gmail.com>
2025-09-08 09:51:06 +0200

    Refactored tests and added custom input back to template

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -6,41 +6,29 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
-class ItemForm(forms.models.ModelForm):
-    class Meta:
-        model = Item
-        fields = ("text",)
-        widgets = {
-            "text": forms.widgets.TextInput(
-                attrs={
-                    "placeholder": "Enter a to-do item",
-                    "class": "form-control form-control-lg",
-                }
-            )
-        }
-        error_messages = {"text": {"required": EMPTY_ITEM_ERROR}}
-        
-    def is_valid(self):
-        result = super().is_valid()
-        if not result:
-            self.fields['text'].widget.attrs["class"] += " is-invalid"
-        return result
+class ItemForm(forms.Form):
+    text = forms.CharField(
+        error_messages={"required": EMPTY_ITEM_ERROR},
+        required=True,
+    )
         
     def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
+        return Item.objects.create(
+            list=for_list,
+            text=self.cleaned_data["text"],
+        )
 
 
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.instance.list = for_list
+        self._for_list = for_list
         
     def clean_text(self):
         text = self.cleaned_data["text"]
-        if self.instance.list.item_set.filter(text=text).exists():
+        if self._for_list.item_set.filter(text=text).exists():
             raise forms.ValidationError(DUPLICATE_ITEM_ERROR)
         return text
     
     def save(self):
-        return forms.models.ModelForm.save(self)
+        return super().save(for_list=self._for_list)
--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -9,28 +9,11 @@ from lists.forms import (
 from lists.models import Item, List
 
 
-class ItemFormTest(TestCase):
-    def test_form_item_input_has_placeholder_and_css_classes(self):
-        form = ItemForm()
-        
-        rendered = form.as_p()
-        
-        self.assertIn('placeholder="Enter a to-do item"', rendered)
-        self.assertIn('class="form-control form-control-lg"', rendered)
-        
+class ItemFormTest(TestCase):      
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={"text": ""})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors["text"], [EMPTY_ITEM_ERROR])
-        
-    def test_invalid_form_has_bootstrap_is_invalid_css_class(self):
-        form = ItemForm(data={"text": ""})
-        self.assertFalse(form.is_valid())
-        field = form.fields["text"]
-        self.assertEqual(
-            field.widget.attrs["class"],
-            "form-control form-control-lg is-invalid"
-        )
         
     def test_form_save_handles_saving_to_a_list(self):
         mylist = List.objects.create()
@@ -41,12 +24,7 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item.text, "do me")
         self.assertEqual(new_item.list, mylist)
         
-class ExistingListItemFormTest(TestCase):
-    def test_form_renders_item_text_input(self):
-        list_ = List.objects.create()
-        form = ExistingListItemForm(for_list=list_)
-        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
-        
+class ExistingListItemFormTest(TestCase):        
     def test_form_save(self):
         mylist = List.objects.create()
         form = ExistingListItemForm(for_list=mylist, data={"text": "hi"})
